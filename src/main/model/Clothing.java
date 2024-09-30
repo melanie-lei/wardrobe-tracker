cb8fd3275162ea3c3cf1ddc616bbd3a06e694e1f
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2024-09-30 13:29:29 -0700

    Added specifications for Clothing class

--- src/main/model/Clothing.java
+++ src/main/model/Clothing.java
@@ -1,14 +1,52 @@
 package model;
+import java.awt.Color;
 
 // A Clothing item that keeps track of its own properties:
-// type, colour, times worn
+// type, colour, totalTimesWorn, timesWornSinceWash, isFavourite
 public class Clothing {
-    enum Type {
+    public enum ClothingType {
         TOP,
         BOTTOM,
         SHOES,
         HEADWEAR,
         ACCESSORY
+    }
+
+    // EFFECTS: creates a Clothing item with:
+    // a specified ClothingType, colour, totalTimesWorn = 0, timesWornSinceWash = 0, isFavourite = false
+    public Clothing(ClothingType t, Color c){
+    }
+
+    // MODIFIES: this
+    // EFFECTS: increments totalTimesWorn and timeWornSinceWash
+    public void wear(){
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: indicates this clothing item as washed and resets times worn since last wash
+    public void wash(){
+        //stub
+    }
+
+    public void setFavourite(){
+        //stub
+    }
+
+    public boolean isFavourite(){
+        return false; //stub
+    }
+
+    public int getTimesWornSinceWash(){
+        return 0; //stub
+    }
+
+    public int getTotalTimesWorn(){
+        return 0; //stub
+    }
+
+    public ClothingType getClothingType(){
+        return null; //stub
     }
 
 }
