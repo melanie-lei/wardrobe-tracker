e17503dc75c2a972d093d78603d676ad51a71fa8
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2024-09-30 15:56:22 -0700

    Added specifications for Wardrobe class

--- src/main/model/Wardrobe.java
+++ src/main/model/Wardrobe.java
@@ -1,6 +1,32 @@
 package model;
 
-// A wardrobe which consists of Outfits
+import java.util.List;
+
+// A wardrobe which consists of Outfits and Clothing items organized by type
 public class Wardrobe {
     
+    // EFFECTS: Creates a Wardrobe with no outfits or clothing items
+    public Wardrobe(){
+        //stub
+    }
+
+    // REQUIRES: category can only be:
+    // "totalTimesWorn", "timesWornSinceWash", "isFavourite", "alphabetical"
+    // MODIFIES: list
+    // EFFECTS: sorts the given list based on the given category
+    public void sortClothing(List<Clothing> list, String category){
+        //stub
+    }
+
+    public List<Outfit> getOutfits(){
+        return null; //stub
+    }
+
+    public List<Clothing> getClothing(){
+        return null; //stub
+    }
+
+    public List<Clothing> getClothing(Clothing.ClothingType t){
+        return null; //stub
+    }
 }
