840a90b33dfba061a836bba5e9ff4d332d90939a
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2024-09-30 16:16:07 -0700

    Added specifications for Outfit class

--- src/main/model/Outfit.java
+++ src/main/model/Outfit.java
@@ -1,6 +1,85 @@
 package model;
 
+import java.util.List;
+
 // An outfit which is made up of Clothing items
 public class Outfit {
     
+    // Effects: creates and Outfit with no Clothing items
+    public Outfit(){
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds an accessory clothing to current accessories
+    public void addAccessory(Clothing c){
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes an accessory clothing from current accessories
+    public void removeAccessory(Clothing c){
+        //stub
+    }
+
+
+    public void setTop(Clothing c){
+        //stub
+    }
+
+    public void setJacket(Clothing c){
+        //stub
+    }
+
+    public void setBottoms(Clothing c){
+        //stub
+    }
+
+    public void setShoes(Clothing c){
+        //stub
+    }
+
+    public void setHeadwear(Clothing c){
+        //stub
+    }
+
+    public Clothing getTop(){
+        return null;//stub
+    }
+
+    public Clothing getJacket(){
+        return null;//stub
+    }
+
+    public Clothing getBottoms(){
+        return null;//stub
+    }
+
+    public Clothing getShoes(){
+        return null;//stub
+    }
+
+    public Clothing getHeadwear(){
+        return null;//stub
+    }
+
+    public List<Clothing> getAccessories(){
+        return null;//stub
+    }
+
+    public void setName(String n){
+        //stub
+    }
+
+    public void setDescription(String d){
+        //stub
+    }
+
+    public String getName(){
+        return "";//stub
+    }
+
+    public String getDescription(){
+        return "";//stub
+    }
+
 }
