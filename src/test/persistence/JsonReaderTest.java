e7ca1943af30a77873b15cc260e44ee2d8eb87e2
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2024-10-19 21:03:24 -0700

    Changed calls to checkClothing in JsonReaderTest

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -52,17 +52,17 @@ class JsonReaderTest extends JsonTest {
             List<Clothing> clothing = w.getClothing();
             assertEquals(6, clothing.size());
             checkClothing(0, "blue shirt", "this is my shirt",
-                    new Color(Integer.parseInt("-16776961")), 4, 2, clothing.get(0));
+                    new Color(Integer.parseInt("-16776961")), 4, 2, true, clothing.get(0));
             checkClothing(1, "red pants", "these are my pants",
-                    new Color(Integer.parseInt("-65536")), 6, 1, clothing.get(1));
+                    new Color(Integer.parseInt("-65536")), 6, 1, false, clothing.get(1));
             checkClothing(2, "red jacket", "my jacket",
-                    new Color(Integer.parseInt("-65536")), 6, 2, clothing.get(2));
+                    new Color(Integer.parseInt("-65536")), 6, 2, false, clothing.get(2));
             checkClothing(3, "red shoes", "these are my shoes",
-                    new Color(Integer.parseInt("-65536")), 5, 1, clothing.get(3));
+                    new Color(Integer.parseInt("-65536")), 5, 1, false, clothing.get(3));
             checkClothing(4, "red hat", "this is my hat",
-                    new Color(Integer.parseInt("-65536")), 3, 1, clothing.get(4));
+                    new Color(Integer.parseInt("-65536")), 3, 1, false, clothing.get(4));
             checkClothing(5, "necklace", "accessory",
-                    new Color(Integer.parseInt("-65536")), 6, 4, clothing.get(5));
+                    new Color(Integer.parseInt("-65536")), 6, 4, false, clothing.get(5));
 
         } catch (IOException e) {
             fail("Couldn't read from file");
