ed76bf7dcb2edb57c23305bd20a4a248ea88ceba
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2024-10-19 21:19:50 -0700

    JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -11,34 +11,39 @@ import model.Wardrobe;
 // SOURCE: JsonWriter example given for phase 2 instructions
 // a writer that writes JSON representation the wardrobe to file
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-
+        this.destination = destination;
     }
 
     // MODIFIES: this
-    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file
+    // cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of wardrobe to file
-    public void write(Wardrobe wr) {
-
+    public void write(Wardrobe wd) {
+        JSONObject json = wd.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-
+        writer.print(json);
     }
 }
