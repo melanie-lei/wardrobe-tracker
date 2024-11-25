6a35fc732efaf2d5f4a178ff2c0b3876c16d1df8
melanie-lei <65362046+melanie-lei@users.noreply.github.com>
2024-11-25 11:47:39 -0800

    Checkstyle changes

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -12,32 +12,32 @@ import org.junit.jupiter.api.Test;
 
 // Tests for Event
 public class EventTest {
-    
-	private Event e;
-	private Date d;
-	
-	//NOTE: these tests might fail if time at which line (2) below is executed
-	//is different from time that line (1) is executed.  Lines (1) and (2) must
-	//run in same millisecond for this test to make sense and pass.
-	
-	@BeforeEach
-	public void runBefore() {
-		e = new Event("Outfit saved.");   // (1)
-		d = Calendar.getInstance().getTime();   // (2)
-	}
-	
-	@Test
-	public void testEvent() {
-		assertEquals("Outfit saved.", e.getDescription());
-		assertEquals(d, e.getDate());
-        assertFalse(e.equals(null));
-        assertFalse(e.equals("Outfit saved"));
-        Event e2 = new Event("Outfit not saved");
-        assertNotEquals(e2.hashCode(), e.hashCode());
-	}
 
-	@Test
-	public void testToString() {
-		assertEquals(d.toString() + "\n" + "Outfit saved.", e.toString());
-	}
+    private Event event;
+    private Date date;
+
+    // NOTE: these tests might fail if time at which line (2) below is executed
+    // is different from time that line (1) is executed. Lines (1) and (2) must
+    // run in same millisecond for this test to make sense and pass.
+
+    @BeforeEach
+    public void runBefore() {
+        event = new Event("Outfit saved."); // (1)
+        date = Calendar.getInstance().getTime(); // (2)
+    }
+
+    @Test
+    public void testEvent() {
+        assertEquals("Outfit saved.", event.getDescription());
+        assertEquals(date, event.getDate());
+        assertFalse(event.equals(null));
+        assertFalse(event.equals("Outfit saved"));
+        Event e2 = new Event("Outfit not saved");
+        assertNotEquals(e2.hashCode(), event.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(date.toString() + "\n" + "Outfit saved.", event.toString());
+    }
 }
